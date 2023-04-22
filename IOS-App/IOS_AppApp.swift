f2ca279002580267015049e2937a79570a987fa4
amora7741 <mora7741@gmail.com>
2023-04-22 13:49:49 -0700

    Initial commit

---
+++ IOS-App/IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_AppApp.swift
+//  IOS-App
+//
+//  Created by Alex Mora on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
